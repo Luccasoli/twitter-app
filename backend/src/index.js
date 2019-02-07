e7d0265a92ed9ffd5cee349c0c891ed1770286e6
luccasoli <oliveira.mesquita@aluno.uece.br>
2019-02-06 23:19:19 -0300

    Iniciando com express/node

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+// Integraçao com banco de dados
+mongoose.connect('mongodb://luccasoli:admin123@ds225375.mlab.com:25375/omni_lucas',
+{
+    useNewUrlParser: true
+})
+
+app.use(require('./routes'))
+
+app.listen(3000, () => {
+    console.log('Servidor iniciado na porta 3000')
+})
